20a23bd32318c74561730ff1bbb5b4f11e2c4b41
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-18 00:46:38 +0300

    edited

--- app/src/main/java/com/chatapp/view/MessagesFragment.kt
+++ app/src/main/java/com/chatapp/view/MessagesFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.chatapp.R
 import com.chatapp.databinding.FragmentMessagesBinding
 
 class MessagesFragment : Fragment() {
