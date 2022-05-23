08006e96f4cbf4904aa3af04344898ca3572d20b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-24 01:00:16 +0300

    dialogs added

---
+++ app/src/main/java/com/chatapp/view/dialogs/AddUserDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.chatapp.view.dialogs
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.chatapp.R
+
+class AddUserDialogFragment : DialogFragment() {
+
+    private lateinit var rootView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        rootView = inflater.inflate(R.layout.custom_adduser_dialog, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rootView.findViewById<Button>(R.id.dialogAddUserButtonAUD).setOnClickListener {
+            // add user
+            dismiss()
+        }
+
+        rootView.findViewById<Button>(R.id.dialogCancelButtonAUD).setOnClickListener {
+            dismiss()
+        }
+    }
+}
