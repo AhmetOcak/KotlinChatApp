77e43cbe6497481cf69f64d009a11d16295c5d86
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-23 15:16:40 +0300

    refactored

--- app/src/main/java/com/chatapp/view/LoginFragment.kt
+++ app/src/main/java/com/chatapp/view/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.chatapp.R
 import com.chatapp.databinding.FragmentLoginBinding
 
@@ -18,5 +19,16 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signUp.setOnClickListener {
+            goToRegisterScreen()
+        }
+    }
+
+    private fun goToRegisterScreen() {
+        findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
     }
 }
