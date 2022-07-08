151d88a22c687afe492822b5595d80f614cb572a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-08 17:49:02 +0300

    user model refactored

--- domain/src/main/java/com/ahmet/domain/entity/UserEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.ahmet.domain.entity
-
-data class UserEntity(
-    val full_name: String,
-    val email_address: String,
-    val password: String
-)
---
+++ domain/src/main/java/com/ahmet/domain/model/User.kt
@@ -0,0 +1,7 @@
+package com.ahmet.domain.model
+
+data class User(
+    val userName: String,
+    val emailAddress: String,
+    val password: String
+)
