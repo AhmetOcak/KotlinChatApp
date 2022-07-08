5db696116837e26117cfb1b5ec6d5468772eac29
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-08 17:50:07 +0300

    register operation end

--- features/src/main/java/com/ahmet/features/register/RegisterFragment.kt
+++ features/src/main/java/com/ahmet/features/register/RegisterFragment.kt
@@ -1,20 +1,78 @@
 package com.ahmet.features.register
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import com.ahmet.core.base.BaseFragment
 import com.ahmet.features.R
 import com.ahmet.features.databinding.FragmentRegisterBinding
+import com.ahmet.features.utils.Constants
+import com.ahmet.features.utils.FirebaseMessages
 
 class RegisterFragment : BaseFragment<RegisterViewModel, FragmentRegisterBinding>() {
 
     override fun getViewModelClass() = RegisterViewModel::class.java
     override fun getViewDataBinding() = FragmentRegisterBinding.inflate(layoutInflater)
+    private lateinit var toast: Toast
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        // Toast definition
+        toast = Toast.makeText(requireContext(), " ", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 0)
+
         binding.registerButton.setOnClickListener {
-            goToNextScreen(R.id.action_registerFragment_to_loginFragment)
+            viewModel.register()
+            setToastMessage()
+            viewModel.message.value = null
+        }
+        observeFirebaseResponse()
+    }
+
+    private fun setToastMessage() {
+        when {
+            viewModel.message.value.toString() == Constants.EMPTY_FIELD_MESSAGE -> {
+                toast.setText(Constants.EMPTY_FIELD_MESSAGE)
+                toast.show()
+            }
+            viewModel.message.value.toString() == Constants.EMAIL_MESSAGE -> {
+                toast.setText(Constants.EMAIL_MESSAGE)
+                toast.show()
+            }
+            viewModel.message.value.toString() == Constants.PASSWORD_MATCH_MESSAGE -> {
+                toast.setText(Constants.PASSWORD_MATCH_MESSAGE)
+                toast.show()
+            }
+            viewModel.message.value.toString() == Constants.PASSWORD_LENGTH_MESSAGE -> {
+                toast.setText(Constants.PASSWORD_LENGTH_MESSAGE)
+                toast.show()
+            }
+        }
+    }
+
+    private fun observeFirebaseResponse() {
+        viewModel.firebaseMessage.observe(viewLifecycleOwner) {
+            when {
+                viewModel.firebaseMessage.value.toString() == FirebaseMessages.EMAIL_ERROR -> {
+                    toast.setText(FirebaseMessages.EMAIL_ERROR)
+                    toast.show()
+                }
+                viewModel.firebaseMessage.value.toString() == FirebaseMessages.NETWORK_ERROR -> {
+                    toast.setText(FirebaseMessages.NETWORK_ERROR)
+                    toast.show()
+                }
+                else -> {
+                    toast.setText(FirebaseMessages.SUCCESSFUL)
+                    toast.show()
+                    goToNextScreen(R.id.action_registerFragment_to_loginFragment)
+                }
+            }
         }
     }
 }
+
+
+
--- features/src/main/java/com/ahmet/features/register/RegisterViewModel.kt
+++ features/src/main/java/com/ahmet/features/register/RegisterViewModel.kt
@@ -1,6 +1,80 @@
 package com.ahmet.features.register
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ahmet.core.base.BaseViewModel
+import com.ahmet.data.usecase.FirebaseOperations
+import com.ahmet.features.utils.CollectionPaths
+import com.ahmet.features.utils.Constants
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class RegisterViewModel : BaseViewModel(){
+class RegisterViewModel : BaseViewModel() {
+
+    val userName = MutableLiveData<String?>()
+    val email = MutableLiveData<String?>()
+    val password = MutableLiveData<String?>()
+    val confirmPassword = MutableLiveData<String?>()
+
+    var message = MutableLiveData<String?>()
+    var firebaseMessage = MutableLiveData<String?>()
+
+    private fun checkTextFields(): Boolean {
+        return if (!userName.value.isNullOrEmpty() && !email.value.isNullOrEmpty() && !password.value.isNullOrEmpty() && !confirmPassword.value.isNullOrEmpty()) {
+            true
+        } else {
+            message.value = Constants.EMPTY_FIELD_MESSAGE
+            false
+        }
+    }
+
+    private fun checkEmail(): Boolean {
+        return if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.value.toString()).matches()) {
+            true
+        } else {
+            message.value = Constants.EMAIL_MESSAGE
+            false
+        }
+    }
+
+    private fun checkPassword(): Boolean {
+        return if (password.value?.length!! < 6) {
+            message.value = Constants.PASSWORD_LENGTH_MESSAGE
+            false
+        } else if (!password.value.equals(confirmPassword.value)) {
+            message.value = Constants.PASSWORD_MATCH_MESSAGE
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun checkRegisterInfo(): Boolean = checkTextFields() && checkEmail() && checkPassword()
+
+    fun register() {
+        viewModelScope.launch {
+            try {
+                if (checkRegisterInfo()) {
+                    firebaseMessage.value = FirebaseOperations().register(
+                        email.value.toString(),
+                        password.value.toString(),
+                        userName.value.toString(),
+                        CollectionPaths.USER
+                    )
+                    clearFields()
+                }
+            } catch (e: Exception) {
+                firebaseMessage.value = e.message
+            }
+        }
+    }
+
+    private fun clearFields() {
+        userName.value = null
+        email.value = null
+        password.value = null
+        confirmPassword.value = null
+        message.value = null
+    }
+
 }
