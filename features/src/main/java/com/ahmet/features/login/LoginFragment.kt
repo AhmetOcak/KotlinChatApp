6c9dd79f0356c561d0a5556b5141c27163c05f73
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-09 18:00:16 +0300

    login end

--- features/src/main/java/com/ahmet/features/login/LoginFragment.kt
+++ features/src/main/java/com/ahmet/features/login/LoginFragment.kt
@@ -1,24 +1,76 @@
 package com.ahmet.features.login
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import com.ahmet.core.base.BaseFragment
 import com.ahmet.features.R
 import com.ahmet.features.databinding.FragmentLoginBinding
+import com.ahmet.features.utils.*
+import kotlinx.coroutines.delay
 
 class LoginFragment : BaseFragment<LoginViewModel, FragmentLoginBinding>() {
 
     override fun getViewModelClass() = LoginViewModel::class.java
     override fun getViewDataBinding() = FragmentLoginBinding.inflate(layoutInflater)
 
+    private lateinit var toast: Toast
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        // Toast definition
+        toast = Toast.makeText(requireContext(), " ", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 0)
 
         binding.signUp.setOnClickListener {
             goToNextScreen(R.id.action_loginFragment_to_registerFragment)
         }
         binding.loginButton.setOnClickListener {
-            goToNextScreen(R.id.action_loginFragment_to_messagesFragment)
+            viewModel.login()
+            setToastMessage()
+            viewModel.message.value = null
+        }
+        observeFirebaseResponse()
+    }
+
+    private fun setToastMessage() {
+        when {
+            viewModel.message.value.toString() == Constants.EMPTY_FIELD_MESSAGE -> {
+                toast.setText(Constants.EMPTY_FIELD_MESSAGE)
+                toast.show()
+            }
+            viewModel.message.value.toString() == Constants.EMAIL_MESSAGE -> {
+                toast.setText(Constants.EMAIL_MESSAGE)
+                toast.show()
+            }
+            viewModel.message.value.toString() == Constants.PASSWORD_LENGTH_MESSAGE -> {
+                toast.setText(Constants.PASSWORD_LENGTH_MESSAGE)
+                toast.show()
+            }
+        }
+    }
+
+    private fun observeFirebaseResponse() {
+        viewModel.firebaseMessage.observe(viewLifecycleOwner) {
+            when {
+                viewModel.firebaseMessage.value.toString() == FirebaseLoginMessages.ERROR_MESSAGE -> {
+                    toast.setText(FirebaseLoginMessages.LOGIN_ERROR)
+                    toast.show()
+                }
+                viewModel.firebaseMessage.value.toString() == FirebaseCommonMessages.NETWORK_ERROR -> {
+                    toast.setText(FirebaseCommonMessages.NETWORK_ERROR)
+                    toast.show()
+                }
+                else -> {
+                    toast.setText(FirebaseLoginMessages.SUCCESSFUL)
+                    toast.show()
+                    goToNextScreen(R.id.action_loginFragment_to_messagesFragment)
+                }
+            }
         }
     }
 }
--- features/src/main/java/com/ahmet/features/login/LoginViewModel.kt
+++ features/src/main/java/com/ahmet/features/login/LoginViewModel.kt
@@ -1,6 +1,92 @@
 package com.ahmet.features.login
 
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ahmet.core.base.BaseViewModel
+import com.ahmet.core.utils.EmailController
+import com.ahmet.data.usecase.FirebaseOperations
+import com.ahmet.features.utils.Constants
+import com.ahmet.features.utils.Status
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class LoginViewModel: BaseViewModel() {
+class LoginViewModel : BaseViewModel() {
+
+    private val _progressBarVisibility = MutableLiveData(View.INVISIBLE)
+    val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
+
+    val email = MutableLiveData<String?>()
+    val password = MutableLiveData<String?>()
+    val rememberMeCheckBox = MutableLiveData(false)
+
+    var message = MutableLiveData<String?>()
+    var firebaseMessage = MutableLiveData<String?>()
+
+    private fun checkFields(): Boolean {
+        return if (!email.value.isNullOrEmpty() && !password.value.isNullOrEmpty()) {
+            true
+        } else {
+            message.value = Constants.EMPTY_FIELD_MESSAGE
+            false
+        }
+    }
+
+    private fun checkEmail(): Boolean {
+        return if (EmailController.emailController(email.value.toString())) {
+            true
+        } else {
+            message.value = Constants.EMAIL_MESSAGE
+            false
+        }
+    }
+
+    private fun checkPassword(): Boolean {
+        return if (password.value?.length!! < 6) {
+            message.value = Constants.PASSWORD_LENGTH_MESSAGE
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun checkLoginInfo(): Boolean = checkFields() && checkEmail() && checkPassword()
+
+    fun login() {
+        viewModelScope.launch {
+            try {
+                if (checkLoginInfo()) {
+                    setProgBarVis(Status.LOADING)
+                    firebaseMessage.value =
+                        FirebaseOperations().login(
+                            email.value.toString(),
+                            password.value.toString()
+                        )
+                    clearFields()
+                    setProgBarVis(Status.DONE)
+                }
+            } catch (e: FirebaseAuthInvalidUserException) {
+                firebaseMessage.value = e.message
+                setProgBarVis(Status.ERROR)
+            } catch (e: Exception) {
+                firebaseMessage.value = e.message
+                setProgBarVis(Status.ERROR)
+            }
+        }
+    }
+
+    private fun setProgBarVis(status: Status) {
+        if (status == Status.LOADING) _progressBarVisibility.value = View.VISIBLE
+        else _progressBarVisibility.value = View.INVISIBLE
+    }
+
+    private fun clearFields() {
+        email.value = null
+        password.value = null
+        message.value = null
+    }
+
+
 }
