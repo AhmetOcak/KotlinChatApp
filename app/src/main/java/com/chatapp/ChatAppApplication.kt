ca9ab405e6b1a330c683e661dffc7ec27a1f588b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-09 22:48:02 +0300

    hilt context bug solved

--- app/src/main/java/com/chatapp/ChatAppApplication.kt
+++ app/src/main/java/com/chatapp/ChatAppApplication.kt
@@ -1,7 +1,13 @@
 package com.chatapp
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ChatAppApplication : Application()
+class ChatAppApplication : Application() {
+
+    override fun getApplicationContext(): Context {
+        return super.getApplicationContext()
+    }
+}
