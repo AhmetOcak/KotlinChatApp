109f795020dc48923e9a0d54a3adc8539a0ca4e8
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-09 16:21:02 +0300

    user mapper created

---
+++ data/src/main/java/com/ahmet/data/mapper/UserMapper.kt
@@ -0,0 +1,24 @@
+package com.ahmet.data.mapper
+
+import com.ahmet.data.model.UserEntity
+import com.ahmet.domain.interfaces.IEntityMapper
+import com.ahmet.domain.model.User
+import javax.inject.Inject
+
+class UserMapper @Inject constructor() : IEntityMapper<UserEntity, User> {
+    override fun mapFromEntity(entity: UserEntity): User {
+        return User(
+            entity.userName,
+            entity.emailAddress,
+            entity.password
+        )
+    }
+
+    override fun entityFromModel(model: User): UserEntity {
+        return UserEntity(
+            model.userName,
+            model.emailAddress,
+            model.password
+        )
+    }
+}
