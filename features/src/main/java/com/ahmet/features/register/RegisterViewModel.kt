303a3072eac3e3cc201c4cbea76924466ea9a8fa
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-09 14:07:33 +0300

    progress bar added

--- features/src/main/java/com/ahmet/features/register/RegisterViewModel.kt
+++ features/src/main/java/com/ahmet/features/register/RegisterViewModel.kt
@@ -1,11 +1,14 @@
 package com.ahmet.features.register
 
+import android.view.View
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.ahmet.core.base.BaseViewModel
 import com.ahmet.data.usecase.FirebaseOperations
 import com.ahmet.features.utils.CollectionPaths
 import com.ahmet.features.utils.Constants
+import com.ahmet.features.utils.Status
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -18,6 +21,9 @@ class RegisterViewModel : BaseViewModel() {
 
     var message = MutableLiveData<String?>()
     var firebaseMessage = MutableLiveData<String?>()
+
+    private val _progressBarVisibility = MutableLiveData(View.INVISIBLE)
+    val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
 
     private fun checkTextFields(): Boolean {
         return if (!userName.value.isNullOrEmpty() && !email.value.isNullOrEmpty() && !password.value.isNullOrEmpty() && !confirmPassword.value.isNullOrEmpty()) {
@@ -55,6 +61,7 @@ class RegisterViewModel : BaseViewModel() {
         viewModelScope.launch {
             try {
                 if (checkRegisterInfo()) {
+                    setProgBarVis(Status.LOADING)
                     firebaseMessage.value = FirebaseOperations().register(
                         email.value.toString(),
                         password.value.toString(),
@@ -64,6 +71,7 @@ class RegisterViewModel : BaseViewModel() {
                     clearFields()
                 }
             } catch (e: Exception) {
+                setProgBarVis(Status.ERROR)
                 firebaseMessage.value = e.message
             }
         }
@@ -75,6 +83,11 @@ class RegisterViewModel : BaseViewModel() {
         password.value = null
         confirmPassword.value = null
         message.value = null
+    }
+
+    private fun setProgBarVis(status: Status) {
+        if(status == Status.LOADING) _progressBarVisibility.value = View.VISIBLE
+        else _progressBarVisibility.value = View.INVISIBLE
     }
 
 }
