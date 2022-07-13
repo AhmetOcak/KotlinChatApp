6af1fa65afaac9b1fbdc4e366f12e5f75b10c6d3
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-13 14:48:00 +0300

    converter created for db

---
+++ data/src/main/java/com/ahmet/data/utils/converters/Converter.kt
@@ -0,0 +1,20 @@
+package com.ahmet.data.utils.converters
+
+import androidx.room.TypeConverter
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+
+
+class Converter {
+
+    @TypeConverter
+    fun fromString(str: String): List<String> {
+        
+        val listType =  object : TypeToken<List<String>>(){}.type
+        return Gson().fromJson(str, listType)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<String>): String = Gson().toJson(list)
+
+}
