9cad9c113f7946c4e015f9b0a87671b069d12d8e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-17 22:53:27 +0300

    return type changed

--- data/src/main/java/com/ahmet/data/usecase/userdatabase/GetUserFromDb.kt
+++ data/src/main/java/com/ahmet/data/usecase/userdatabase/GetUserFromDb.kt
@@ -2,11 +2,13 @@ package com.ahmet.data.usecase.userdatabase
 
 import com.ahmet.data.local.db.UserDatabase
 import com.ahmet.data.local.db.entity.UserDataEntity
+import com.ahmet.data.mapper.UserDataMapper
+import com.ahmet.domain.model.User
 import javax.inject.Inject
 
 class GetUserFromDb @Inject constructor(private val db: UserDatabase) {
 
-    fun getUser(): UserDataEntity? {
-        return db.userDao().getUser()
+    fun getUser(): User? {
+        return db.userDao().getUser()?.let { UserDataMapper().mapFromEntity(it) }
     }
 }
