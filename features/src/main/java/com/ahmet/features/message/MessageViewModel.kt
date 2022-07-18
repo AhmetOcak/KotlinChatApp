ca61506920d16431097d5e14909cb215769724c8
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-18 18:14:06 +0300

    user friends got

--- features/src/main/java/com/ahmet/features/message/MessageViewModel.kt
+++ features/src/main/java/com/ahmet/features/message/MessageViewModel.kt
@@ -1,8 +1,66 @@
 package com.ahmet.features.message
 
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ahmet.core.base.BaseViewModel
+import com.ahmet.data.usecase.firebase.GetUserData
+import com.ahmet.data.usecase.userdatabase.GetUserFromDb
+import com.ahmet.domain.model.User
+import com.ahmet.features.utils.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MessageViewModel(): BaseViewModel() {
+@HiltViewModel
+class MessageViewModel @Inject constructor(
+    private val getUser: GetUserData,
+    private val getUserFromDb: GetUserFromDb
+) : BaseViewModel() {
 
+    var userEmail = MutableLiveData<String>()
+
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User> get() = _user
+
+    private val _progressBarVisibility = MutableLiveData(View.INVISIBLE)
+    val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
+
+    private val _userFriends = MutableLiveData<MutableList<User>>(mutableListOf())
+    val userFriends: LiveData<MutableList<User>> get() = _userFriends
+
+    // if mail not null --> the user has not checked the remember me box.
+    // if mail null --> the user has checked the remember me box.
+    fun setUserData() {
+        setProgBarVis(Status.LOADING)
+        if (!userEmail.value.isNullOrEmpty()) {
+            viewModelScope.launch {
+                _user.value = getUser.getUserDoc(userEmail.value.toString())
+                getUserFriendsData()
+            }
+        } else {
+            _user.value = getUserFromDb.getUser()
+            getUserFriendsData()
+        }
+    }
+
+    private fun getUserFriendsData() {
+        viewModelScope.launch {
+            if (_user.value != null && _userFriends.value.isNullOrEmpty()) {
+                for (i in 0 until (_user.value?.userFriends?.size ?: 0)) {
+                    getUser.getUserDoc(_user.value!!.userFriends[i])
+                        ?.let { _userFriends.value?.add(it) }
+                }
+            }
+            setProgBarVis(Status.DONE)
+        }
+    }
+
+    private fun setProgBarVis(status: Status) {
+        if (status == Status.LOADING) _progressBarVisibility.value = View.VISIBLE
+        else _progressBarVisibility.value = View.INVISIBLE
+    }
 }
+
