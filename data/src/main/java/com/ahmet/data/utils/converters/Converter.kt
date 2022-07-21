50dde042aa4f8e091c4a6c28f6ea2f9a7feda3ec
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-21 14:20:36 +0300

    comment line added

--- data/src/main/java/com/ahmet/data/utils/converters/Converter.kt
+++ data/src/main/java/com/ahmet/data/utils/converters/Converter.kt
@@ -4,12 +4,11 @@ import androidx.room.TypeConverter
 import com.google.common.reflect.TypeToken
 import com.google.gson.Gson
 
-
+// type converter for user database
 class Converter {
 
     @TypeConverter
     fun fromString(str: String): List<String> {
-        
         val listType =  object : TypeToken<List<String>>(){}.type
         return Gson().fromJson(str, listType)
     }
