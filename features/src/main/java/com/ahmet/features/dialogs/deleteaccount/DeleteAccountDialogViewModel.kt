f8d300ab99c6d5426ba9fdb44de7ff25efbc8710
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-22 15:19:49 +0300

    delete account | log out vm created

---
+++ features/src/main/java/com/ahmet/features/dialogs/deleteaccount/DeleteAccountDialogViewModel.kt
@@ -0,0 +1,10 @@
+package com.ahmet.features.dialogs.deleteaccount
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DeleteAccountDialogViewModel @Inject constructor(): ViewModel() {
+
+}
---
+++ features/src/main/java/com/ahmet/features/dialogs/logout/LogOutViewModel.kt
@@ -0,0 +1,6 @@
+package com.ahmet.features.dialogs.logout
+
+import com.ahmet.core.base.BaseViewModel
+
+class LogOutViewModel: BaseViewModel() {
+}
