7a6c0a129084f4f7ae48b5417937c9b1f6300955
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-24 17:16:58 +0300

    message date listening

--- features/src/main/java/com/ahmet/features/chat/ChatFragment.kt
+++ features/src/main/java/com/ahmet/features/chat/ChatFragment.kt
@@ -1,11 +1,76 @@
 package com.ahmet.features.chat
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 import com.ahmet.core.base.BaseFragment
+import com.ahmet.features.adapter.IncomingMessageAdapter
+import com.ahmet.features.adapter.OutgoingMessageAdapter
 import com.ahmet.features.databinding.FragmentChatBinding
+import com.ahmet.features.utils.Constants
+import com.google.android.flexbox.AlignItems
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class ChatFragment : BaseFragment<ChatViewModel, FragmentChatBinding>() {
 
     override fun getViewModelClass() = ChatViewModel::class.java
     override fun getViewDataBinding() = FragmentChatBinding.inflate(layoutInflater)
+
+    lateinit var incomingMessageAdapter: IncomingMessageAdapter
+    private lateinit var outgoingMessageAdapter: OutgoingMessageAdapter
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        viewModel.friendEmail.value = requireArguments().getString(Constants.USER_FRIEND_ARG_KEY)
+
+        viewModel.getMessageData()
+        viewModel.progressBarVisibility.observe(viewLifecycleOwner) {
+            if (viewModel.progressBarVisibility.value == View.INVISIBLE) {
+                setupAdapters()
+            }
+        }
+
+        viewModel.userMessages.observe(viewLifecycleOwner) {
+            if (viewModel.progressBarVisibility.value == View.INVISIBLE) {
+                outgoingMessageAdapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.friendMessages.observe(viewLifecycleOwner) {
+            if (viewModel.progressBarVisibility.value == View.INVISIBLE) {
+                incomingMessageAdapter.notifyDataSetChanged()
+            }
+
+        }
+    }
+
+    private fun setupAdapters() {
+        val incomingMA = FlexboxLayoutManager(activity)
+        val outgoingMA = FlexboxLayoutManager(activity)
+
+        binding.incomingMessageRecylerview.layoutManager = incomingMA
+        incomingMA.flexDirection = FlexDirection.COLUMN
+        incomingMA.justifyContent = JustifyContent.FLEX_START
+        incomingMA.alignItems = AlignItems.FLEX_START
+
+        binding.outgoingMessageRecylerview.layoutManager = outgoingMA
+        outgoingMA.flexDirection = FlexDirection.COLUMN
+        outgoingMA.justifyContent = JustifyContent.FLEX_START
+        outgoingMA.alignItems = AlignItems.FLEX_END
+
+        incomingMessageAdapter = IncomingMessageAdapter(viewModel.friendMessages.value ?: listOf())
+        outgoingMessageAdapter = OutgoingMessageAdapter(viewModel.userMessages.value ?: listOf())
+
+        binding.incomingMessageRecylerview.adapter = incomingMessageAdapter
+        binding.outgoingMessageRecylerview.adapter = outgoingMessageAdapter
+    }
 
 }
--- features/src/main/java/com/ahmet/features/chat/ChatViewModel.kt
+++ features/src/main/java/com/ahmet/features/chat/ChatViewModel.kt
@@ -1,8 +1,61 @@
 package com.ahmet.features.chat
 
-import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ahmet.core.base.BaseViewModel
+import com.ahmet.data.usecase.firebase.GetCurrentUserEmail
+import com.ahmet.data.usecase.messages.ListenMessageData
+import com.ahmet.domain.model.Message
+import com.ahmet.features.utils.Status
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ChatViewModel : BaseViewModel() {
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+    getCurrentUserEmail: GetCurrentUserEmail,
+    private val listenMessageData: ListenMessageData
+) : BaseViewModel() {
+
+    val friendEmail = MutableLiveData<String>()
+    private val userEmail = MutableLiveData<String>()
+
+    init {
+        userEmail.value = getCurrentUserEmail.getCurrentUser()
+    }
+
+    private val _progressBarVisibility = MutableLiveData(View.GONE)
+    val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
+
+    private val _messages = MutableLiveData<Message>()
+    val messages: LiveData<Message> get() = _messages
+
+    private val _userMessages = MutableLiveData<List<Map<String, Any>>>()
+    val userMessages: LiveData<List<Map<String, Any>>> get() = _userMessages
+
+    private val _friendMessages = MutableLiveData<List<Map<String, Any>>>()
+    val friendMessages: LiveData<List<Map<String, Any>>> get() = _friendMessages
+
+    fun getMessageData() {
+        setProgressBarVis(Status.LOADING)
+
+        listenMessageData.listenData(
+            userEmail.value.toString(),
+            friendEmail.value.toString(),
+            ::callbackFun
+        )
+    }
+
+    private fun setProgressBarVis(status: Status) {
+        if (status == Status.LOADING) _progressBarVisibility.value = View.VISIBLE
+        else _progressBarVisibility.value = View.INVISIBLE
+    }
+
+    private fun callbackFun(message: Message?) {
+        _messages.value = message ?: Message(listOf(), listOf())
+        _userMessages.value = _messages.value?.userMessage ?: listOf()
+        _friendMessages.value = _messages.value?.friendMessage ?: listOf()
+        setProgressBarVis(Status.DONE)
+    }
 
 }
