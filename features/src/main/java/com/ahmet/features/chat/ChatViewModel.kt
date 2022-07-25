2eaacd33bcccf47592f4a2017e276594377668af
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-25 22:35:55 +0300

    send message func implemented

--- features/src/main/java/com/ahmet/features/chat/ChatViewModel.kt
+++ features/src/main/java/com/ahmet/features/chat/ChatViewModel.kt
@@ -1,24 +1,30 @@
 package com.ahmet.features.chat
 
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ahmet.core.base.BaseViewModel
 import com.ahmet.data.usecase.firebase.GetCurrentUserEmail
 import com.ahmet.data.usecase.messages.ListenMessageData
+import com.ahmet.data.usecase.messages.SendMessage
 import com.ahmet.domain.model.Message
 import com.ahmet.features.utils.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     getCurrentUserEmail: GetCurrentUserEmail,
-    private val listenMessageData: ListenMessageData
+    private val listenMessageData: ListenMessageData,
+    private val sendMessage: SendMessage
 ) : BaseViewModel() {
 
-    val friendEmail = MutableLiveData<String>()
     private val userEmail = MutableLiveData<String>()
+    val friendEmail = MutableLiveData<String>()
+    val message = MutableLiveData<String>()
 
     init {
         userEmail.value = getCurrentUserEmail.getCurrentUser()
@@ -27,8 +33,8 @@ class ChatViewModel @Inject constructor(
     private val _progressBarVisibility = MutableLiveData(View.GONE)
     val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
 
-    private val _messages = MutableLiveData<Message>()
-    val messages: LiveData<Message> get() = _messages
+    private val _messageData = MutableLiveData<Message>()
+    val messageData: LiveData<Message> get() = _messageData
 
     private val _userMessages = MutableLiveData<List<Map<String, Any>>>()
     val userMessages: LiveData<List<Map<String, Any>>> get() = _userMessages
@@ -39,11 +45,30 @@ class ChatViewModel @Inject constructor(
     fun getMessageData() {
         setProgressBarVis(Status.LOADING)
 
-        listenMessageData.listenData(
-            userEmail.value.toString(),
-            friendEmail.value.toString(),
-            ::callbackFun
-        )
+        viewModelScope.launch {
+            listenMessageData.listenData(
+                userEmail.value.toString(),
+                friendEmail.value.toString(),
+                ::callbackFun
+            )
+        }
+    }
+
+    fun sendMessageData() {
+        if (checkMessage()) {
+            viewModelScope.launch {
+                try {
+                    sendMessage.sendMessage(
+                        message.value.toString(),
+                        userEmail.value!!,
+                        friendEmail.value!!,
+                        1658742397
+                    )
+                } catch (e: Exception) {
+                    Log.e("message send exception", e.message.toString())
+                }
+            }
+        }
     }
 
     private fun setProgressBarVis(status: Status) {
@@ -52,10 +77,14 @@ class ChatViewModel @Inject constructor(
     }
 
     private fun callbackFun(message: Message?) {
-        _messages.value = message ?: Message(listOf(), listOf())
-        _userMessages.value = _messages.value?.userMessage ?: listOf()
-        _friendMessages.value = _messages.value?.friendMessage ?: listOf()
+        _messageData.value = message ?: Message(listOf(), listOf())
+        _userMessages.value = _messageData.value?.userMessage ?: listOf()
+        _friendMessages.value = _messageData.value?.friendMessage ?: listOf()
         setProgressBarVis(Status.DONE)
+    }
+
+    private fun checkMessage(): Boolean {
+        return !(message.value.isNullOrEmpty() || message.value == "null" || message.value == " ")
     }
 
 }
