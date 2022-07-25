6d781f9486ffce779ea4f7aa406c2259f5ae8441
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-25 22:36:22 +0300

    fields made nullable

--- domain/src/main/java/com/ahmet/domain/model/Message.kt
+++ domain/src/main/java/com/ahmet/domain/model/Message.kt
@@ -1,6 +1,6 @@
 package com.ahmet.domain.model
 
 data class Message(
-    val userMessage: List<Map<String, Any>>,
-    val friendMessage: List<Map<String, Any>>
+    val userMessage: List<Map<String, Any>>?,
+    val friendMessage: List<Map<String, Any>>?
 )
