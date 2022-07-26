1dfd39f00e3e42b6946544d9bb278d9ecf9a6897
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-26 20:12:50 +0300

    new func added

--- data/src/main/java/com/ahmet/data/utils/EditEmail.kt
+++ data/src/main/java/com/ahmet/data/utils/EditEmail.kt
@@ -2,4 +2,10 @@ package com.ahmet.data.utils
 
 object EditEmail {
     fun edit(email: String): String = email.replace(".", "")
+
+    fun addDot(email: String): String {
+        val editedStr = email.substring(email.indexOf('@') + 1, email.indexOf('@') + 6) + ".com"
+        val firstStr = email.substring(0, email.indexOf('@') + 1)
+        return firstStr + editedStr
+    }
 }
