0b13bf9b70b718a9c9c63e454adeec273a8bee7e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-27 23:26:33 +0300

    non-friend message catch method added | doc path refactored

--- data/src/main/java/com/ahmet/data/repository/FirebaseMessagesRepository.kt
+++ data/src/main/java/com/ahmet/data/repository/FirebaseMessagesRepository.kt
@@ -10,6 +10,7 @@ import com.google.firebase.firestore.*
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
+@Suppress("ConvertToStringTemplate")
 class FirebaseMessagesRepository @Inject constructor() : IFirebaseMessagesRepository {
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
@@ -23,7 +24,7 @@ class FirebaseMessagesRepository @Inject constructor() : IFirebaseMessagesReposi
             data[EditEmail.edit(userEmail)] = message.userMessage!!
             data[EditEmail.edit(friendEmail)] = message.friendMessage!!
 
-            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(userEmail + friendEmail)
+            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(userEmail + " " + friendEmail)
                 .set(data)
         }
     }
@@ -43,8 +44,10 @@ class FirebaseMessagesRepository @Inject constructor() : IFirebaseMessagesReposi
 
             if (value != null) {
                 if (!value.data.isNullOrEmpty()) {
-                    val userData = value.data!!.getValue(EditEmail.edit(userEmail)) as List<Map<String, Any>>?
-                    val friendData = value.data!!.getValue(EditEmail.edit(friendEmail)) as List<Map<String, Any>>?
+                    val userData =
+                        value.data!!.getValue(EditEmail.edit(userEmail)) as List<Map<String, Any>>?
+                    val friendData =
+                        value.data!!.getValue(EditEmail.edit(friendEmail)) as List<Map<String, Any>>?
                     callback(Message(userData, friendData))
                 } else {
                     val userData = listOf<Map<String, Any>>()
@@ -73,8 +76,10 @@ class FirebaseMessagesRepository @Inject constructor() : IFirebaseMessagesReposi
 
     private suspend fun isMessageDocCreated(userEmail: String, friendEmail: String): Boolean {
         var exist = false
+        Log.e("first phase", exist.toString())
 
-        db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(friendEmail + userEmail).get()
+        db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(friendEmail + " " + userEmail)
+            .get()
             .addOnCompleteListener {
                 exist = it.result.exists()
             }.await()
@@ -87,9 +92,47 @@ class FirebaseMessagesRepository @Inject constructor() : IFirebaseMessagesReposi
         friendEmail: String
     ): DocumentReference {
         return if (!isMessageDocCreated(userEmail, friendEmail)) {
-            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(userEmail + friendEmail)
+            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(userEmail + " " + friendEmail)
         } else {
-            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(friendEmail + userEmail)
+            db.collection(Firebase.MESSAGES_COLLECTION_PATH).document(friendEmail + " " + userEmail)
         }
     }
+
+    private suspend fun receiveMessageFromNonFriend(userEmail: String): String {
+        var userFriend = "null"
+
+        db.collection(Firebase.MESSAGES_COLLECTION_PATH).get().addOnCompleteListener {
+            for (doc in it.result.documents) {
+                if (doc.id.findWord(userEmail)) {
+                    if (doc.data?.get(doc.data?.keys!!.first()).toString() != "[]") {
+                        userFriend = doc.data!!.keys.first()
+                    }
+                }
+            }
+        }.await()
+
+        return userFriend
+    }
+
+
+    // if return "null" -> the user has already been added to friends or the user has not received any messages.
+    // if return "{user email}" -> the user is not added in friends or the user has a message.
+    suspend fun searchUserFriends(userEmail: String): String {
+        var friendEmail = receiveMessageFromNonFriend(userEmail)
+        var queryResult = true
+
+        if (friendEmail != "null") {
+            queryResult =
+                FirebaseUserDataRepository().isUserAlreadyAdded(userEmail, friendEmail) ?: true
+        }
+
+        return if (queryResult) {
+            friendEmail = "null"
+            friendEmail
+        } else {
+            EditEmail.addDot(friendEmail)
+        }
+    }
+
+    private fun String.findWord(word: String) = "\\b$word\\b".toRegex().containsMatchIn(this)
 }
