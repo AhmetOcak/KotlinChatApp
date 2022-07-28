ed309d22e2da3d4297d6f9d78dc2e790ca1d960d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-28 23:41:33 +0300

    model type changed

--- domain/src/main/java/com/ahmet/domain/model/Message.kt
+++ domain/src/main/java/com/ahmet/domain/model/Message.kt
@@ -1,6 +1,6 @@
 package com.ahmet.domain.model
 
 data class Message(
-    val userMessage: List<Map<String, Any>>?,
-    val friendMessage: List<Map<String, Any>>?
+    val userMessage: MutableList<MutableMap<String, Any>>?,
+    val friendMessage: MutableList<MutableMap<String, Any>>?
 )
