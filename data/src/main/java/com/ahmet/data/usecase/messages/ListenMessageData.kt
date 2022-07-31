55b786f93b33078a9e5e77951b6e3ebf93cbdcae
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-31 23:53:31 +0300

    listen data method updated

--- data/src/main/java/com/ahmet/data/usecase/messages/ListenMessageData.kt
+++ data/src/main/java/com/ahmet/data/usecase/messages/ListenMessageData.kt
@@ -6,11 +6,11 @@ import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class ListenMessageData @Inject constructor(private val repository: FirebaseMessagesRepository) {
-    suspend fun listenData(
+    suspend fun listenPrivateData(
         userEmail: String,
         friendEmail: String,
         callback: (messages: Message?) -> Unit
     ) {
-        repository.listenMessages(userEmail, friendEmail, callback)
+        repository.listenPrivateMessages(userEmail, friendEmail, callback)
     }
 }
