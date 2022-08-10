0131b48982ccee4eac8005727d5a73c42ed1e94e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-08-10 23:46:51 +0300

    accept-decline friend request added

--- features/src/main/java/com/ahmet/features/adapter/FriendRequestsAdapter.kt
+++ features/src/main/java/com/ahmet/features/adapter/FriendRequestsAdapter.kt
@@ -1,20 +1,36 @@
 package com.ahmet.features.adapter
 
 import android.content.SharedPreferences
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.recyclerview.widget.RecyclerView
+import com.ahmet.data.usecase.firebase.AddUser
+import com.ahmet.data.usecase.firebase.DeleteFriendRequest
+import com.ahmet.data.usecase.firebase.GetCurrentUserEmail
+import com.ahmet.data.usecase.messages.CreateMessageDoc
 import com.ahmet.features.databinding.FriendRequestBinding
 import com.ahmet.features.utils.resource.ImpUserImage
+import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 class FriendRequestsAdapter @Inject constructor(
-    private val friendRequest: List<String>,
-    private val sharedPreferences: SharedPreferences
+    private val friendRequest: MutableList<String>,
+    private val sharedPreferences: SharedPreferences,
+    private val addUser: AddUser,
+    private val deleteFriendRequest: DeleteFriendRequest,
+    private val lifecycleScope: LifecycleCoroutineScope,
+    private val getCurrentUserEmail: GetCurrentUserEmail,
+    private val createMessageDoc: CreateMessageDoc
 ) : RecyclerView.Adapter<FriendRequestsAdapter.FriendRequestViewHolder>() {
+
+    private var resultMessageOne: String? = null
+    private var resultMessageTwo: String? = null
 
     class FriendRequestViewHolder(binding: FriendRequestBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -31,9 +47,36 @@ class FriendRequestsAdapter @Inject constructor(
         setFriendImages(friendRequest[position], holder)
         holder.userEmail.text = friendRequest[position]
 
-        holder.acceptButton.setOnClickListener {  }
+        // if friend added successfully then we are deleting friend request
+        holder.acceptButton.setOnClickListener {
+            lifecycleScope.launch {
+                resultMessageOne = addUser.addUser(friendRequest[position], getCurrentUserEmail.getCurrentUser()!!)
+                resultMessageTwo = addUser.addUser(getCurrentUserEmail.getCurrentUser()!!, friendRequest[position])
+                if (resultMessageOne == "Successful" && resultMessageTwo == "Successful" ) {
+                    deleteFriendRequest.deleteRequest(
+                        friendRequest[position],
+                        getCurrentUserEmail.getCurrentUser()!!
+                    )
+                    createMessageDoc.createMessageDoc(getCurrentUserEmail.getCurrentUser()!!, friendRequest[position])
+                    friendRequest.remove(friendRequest[position])
+                    notifyItemRemoved(position)
+                }
+            }
+        }
 
-        holder.declineButton.setOnClickListener {  }
+        holder.declineButton.setOnClickListener {
+            lifecycleScope.launch {
+                try {
+                    val resultMessage = deleteFriendRequest.deleteRequest(friendRequest[position], getCurrentUserEmail.getCurrentUser()!!)
+                    if(resultMessage == "Successful") {
+                        friendRequest.remove(friendRequest[position])
+                        notifyItemRemoved(position)
+                    }
+                }catch (e: Exception) {
+                    Log.e("decline friend request", e.toString())
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int = friendRequest.size
