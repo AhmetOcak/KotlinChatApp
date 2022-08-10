8e48ad8c2ad5808e5eceab2c9eed5ab48aef2a98
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-08-10 23:47:44 +0300

    friend request adapter args added

--- features/src/main/java/com/ahmet/features/friend_requests/FriendRequests.kt
+++ features/src/main/java/com/ahmet/features/friend_requests/FriendRequests.kt
@@ -3,11 +3,15 @@ package com.ahmet.features.friend_requests
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ahmet.core.base.BaseFragment
+import com.ahmet.data.usecase.firebase.AddUser
+import com.ahmet.data.usecase.firebase.DeleteFriendRequest
+import com.ahmet.data.usecase.firebase.GetCurrentUserEmail
+import com.ahmet.data.usecase.messages.CreateMessageDoc
 import com.ahmet.features.adapter.FriendRequestsAdapter
 import com.ahmet.features.databinding.FragmentFriendRequestsBinding
-import com.ahmet.features.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,6 +25,14 @@ class FriendRequests : BaseFragment<FriendRequestsViewModel, FragmentFriendReque
 
     @Inject
     lateinit var sharedPreferences: SharedPreferences
+    @Inject
+    lateinit var addUser: AddUser
+    @Inject
+    lateinit var deleteFriendRequest: DeleteFriendRequest
+    @Inject
+    lateinit var getCurrentUserEmail: GetCurrentUserEmail
+    @Inject
+    lateinit var createMessageDoc: CreateMessageDoc
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,9 +44,17 @@ class FriendRequests : BaseFragment<FriendRequestsViewModel, FragmentFriendReque
 
     private fun initAdapter() {
         viewModel.progressBarVis.observe(viewLifecycleOwner) {
-            if(viewModel.progressBarVis.value == View.INVISIBLE) { // if progress bar gone then our data ready
+            if (viewModel.progressBarVis.value == View.INVISIBLE) { // if progress bar gone then our data ready
                 binding.friendRequests.layoutManager = LinearLayoutManager(activity)
-                adapter = FriendRequestsAdapter(viewModel.friendRequests.value ?: listOf(), sharedPreferences)
+                adapter = FriendRequestsAdapter(
+                    viewModel.friendRequests.value ?: mutableListOf(),
+                    sharedPreferences,
+                    addUser,
+                    deleteFriendRequest,
+                    lifecycleScope,
+                    getCurrentUserEmail,
+                    createMessageDoc
+                )
                 binding.friendRequests.adapter = adapter
             }
         }
